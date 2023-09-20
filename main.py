587fe8bd2790d9531f15e9ff10c5866f6ddec85f
josangkwon <tkdrnjs3@naver.com>
2023-09-20 17:30:44 +0900

    커밋3 복원후 제곱 추가

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1-n2
 
+def mul_func(n1,n2):
+    return n1*n2
+
+def div_func(n1,n2):
+    return n1/n2
+
+def mul2_func(n1,n2):
+    return (n1*n2)^2
+
 ##전역 변수부
 num1 =100
 num2 =200
@@ -19,3 +28,13 @@ print(num1,'+',num2,'=',res)
 res = sub_func(num1,num2)
 
 print(num1,'-',num2,'=',res)
+
+res = mul_func(num1,num2)
+
+print(num1,'*',num2,'=',res)
+
+res = div_func(num1,num2)
+
+print(num1,'/',num2,'=',res)
+res = mul2_func(num1,num2)
+print(num1,'*',num2,'^2','=',res)
